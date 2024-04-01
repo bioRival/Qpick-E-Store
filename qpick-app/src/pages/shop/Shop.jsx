cc9099d14907e32f1f9002e6d453ed0219b7eaae
BioRival <biorivalmain@gmail.com>
2024-04-01 14:01:52 +0300

    v0.7 Cards Category Filter added

--- qpick-app/src/pages/shop/Shop.jsx
+++ qpick-app/src/pages/shop/Shop.jsx
@@ -2,12 +2,38 @@ import { PRODUCTS } from '../../products.js'
 import MerchCard from '../../components/MerchCard.jsx'
 
 export default function Shop() {
+
+    // Let's assume we got dictionary of categories out of DB
+    const categories = {
+        "headphone": "Наушники",
+        "wireless-headphone": "Безпроводные наушники",
+    };
+
+
+    // Filters cards to be under their respected category such as Наушники, Безпроводные наушники
+    // Sends HTML as output
+    function outputProducts() {
+
+        const returnList = [];
+        for (let category in categories) {
+
+            const filteredProducts = PRODUCTS.filter(product => product.category === category);
+
+            // Category title
+            returnList.push(<h2 className='merch-category-title'>{categories[category]}</h2>);
+            
+            // All product cards of that category
+            returnList.push(
+                <div className='merch-container'>
+                    {filteredProducts.map(product => <MerchCard data={product}/>)}
+                </div>);
+        }
+        return returnList;
+    }
+
     return (
-    <div className='merch-area'>    
-        <h2 className='merch-category-title'>Наушники</h2>
-        <div className='merch-container'>
-            {PRODUCTS.map(product => <MerchCard data={product}/>)}
-        </div>  
+    <div className='merch-area'>
+        {outputProducts()}
     </div>
     );
 }
